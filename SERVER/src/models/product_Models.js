3934ab0c213aad990456bfc8fffb4cffeb1aa12c
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-05-01 20:45:07 -0300

    estructura basica de backend, sin datos reales

---
+++ SERVER/src/models/product_Models.js
@@ -0,0 +1,10 @@
+class Products {
+    constructor(id, nombre, detalle, precio, stock, imagen){
+        this.id = id;
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.stock = stock;
+        this.imagen = imagen;
+    }
+}
